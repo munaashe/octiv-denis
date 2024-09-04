bf3ab6958b0ea1ff95b9880985686d32d52494e0
Denis Siduna <denis.siduna@gmail.com>
2024-09-04 12:48:40 +0200

    initialise react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-[100vh] w-full flex justify-center pt-24">
+      and here we go!
+    </div>
+  );
+}
+
+export default App;
